152fabc1db817a607ee127885810094a19ee587a
omolade <amoohaleema226@gmail.com>
2023-04-17 22:09:28 +0100

    added changes

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Apple from "../images/Apple.png";
 import Google from "../images/Google.png";
-import Hero from "../images/Hero.png"
+import Hero from "../images/Hero.png";
 const SecondPage = () => {
   return (
     <div className="overflow-hidden">
       <div class="container mx-0 -my-10  lg:mx-4 lg:-my-2 md:mx-4 md:my-4 bg-fff">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-8">
           <div class="p-0 lg:p-12 md:p-12  mt-10 lg:mt-16">
-            <h2 className="text-3xl lg:text-4xl p-0 ml-2 lg:ml-0 md:ml-0 font-semibold lg:p-2 font-nexa" style={{lineHeight:"64px"}}>Laundry and Dry- <br /> Cleaning got easier!</h2>
+            <h2
+              className="text-3xl lg:text-4xl p-0 ml-2 lg:ml-0 md:ml-0 font-semibold lg:p-2 font-mono"
+              style={{ lineHeight: "64px" }}
+            >
+              Laundry and Dry- <br /> Cleaning got easier!
+            </h2>
             <p className="text-base font-normal font-nexaRegular  p-2">
               We offer convenient pickup and delivery options to easy your
               laundry experience.
@@ -24,7 +29,7 @@ const SecondPage = () => {
             </div>
           </div>
           <div class="p-6 lg:p-4 md:p-4  ">
-        <img src={Hero} alt="" srcset=""  className="hero-image" />
+            <img src={Hero} alt="" srcset="" className="hero-image" />
           </div>
         </div>
       </div>
